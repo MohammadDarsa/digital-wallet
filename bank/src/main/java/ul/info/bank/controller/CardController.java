0cd89022a0888aa6ffcf42d57b1ead0f68ef7bae
Darsa <mohamad.darsa@foo-solutions.com>
2023-02-19 12:54:34 +0200

    changed ResponseFactory to a singleton(by rana)

--- bank/src/main/java/ul/info/bank/controller/CardController.java
+++ bank/src/main/java/ul/info/bank/controller/CardController.java
@@ -13,8 +13,6 @@ import ul.info.bank.service.CardService;
 
 import java.util.List;
 
-import static ul.info.bank.common.util.ResponseFactory.fail;
-import static ul.info.bank.common.util.ResponseFactory.success;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,44 +20,45 @@ import static ul.info.bank.common.util.ResponseFactory.success;
 public class CardController {
 
     private final CardService cardservice;
+    private final ResponseFactory rf = ResponseFactory.getInstance();
 
     @PostMapping("/issue-card")
     public BaseResponse<CardDto> issue(@RequestBody CardDto request) {
-        return success(cardservice.saveCard(request));
+        return rf.success(cardservice.saveCard(request));
     }
 
     @GetMapping("/get-all-cards")
     public BaseResponse<List<Card>> getAll(){
-        return success(cardservice.getAll());
+        return rf.success(cardservice.getAll());
     }
 
     @GetMapping("/get-card-details/{id}")
     public BaseResponse<CardDto> getDetails(@PathVariable("id") Long id){
-        return success(cardservice.getCard(id));
+        return rf.success(cardservice.getCard(id));
     }
 
     @PostMapping("/get-card-details")
     public BaseResponse<CardDto> getDetails(@RequestBody GetCardDetailsRequest request){
-        return success(cardservice.getCard(request.getPan(),request.getCvv(),request.getExp()));
+        return rf.success(cardservice.getCard(request.getPan(),request.getCvv(),request.getExp()));
     }
     //we can use post
      @DeleteMapping("/delete-card/{id}")
      public BaseResponse<?> delete(@PathVariable("id") Long id){
         return (cardservice.deleteCard(id)) ?
-                success(null) :
-                fail("Card not found", "Delete-001", "Please try a different card id");
+                rf.success(null) :
+                rf.fail("Card not found", "Delete-001", "Please try a different card id");
      }
      @PostMapping("/add-amount")
      public BaseResponse<Double> AddAmount(@RequestBody AddAmountRequest request){
          Double res = cardservice.addAmount(request.getId(), request.getAmount());
-         return (res >= 0.0) ? ResponseFactory.success(res) : fail("error adding amount", "AddAmount-001", "try again later");
+         return (res >= 0.0) ? rf.success(res) : rf.fail("error adding amount", "AddAmount-001", "try again later");
      }
 
      @PostMapping("/transfer")
      public BaseResponse<?> transfer(@RequestBody TransferRequest request){
          return cardservice.transfer(request.getFirstCardId(),request.getSecondCardPan(),request.getAmount()) ?
-                 success(null) :
-                 fail("Transfer failed", "Transfer-001", "Please check the values and try again.");
+                 rf.success(null) :
+                 rf.fail("Transfer failed", "Transfer-001", "Please check the values and try again.");
      }
 
 }
