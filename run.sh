3ae4f3eced81327000ba50fa1e5908f837e6d304
darsa <darsafxz@gmail.com>
2023-02-25 00:16:06 +0200

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export PATH=$PATH":/c/Program Files/PostgreSQL/15/bin"
 read -r -p 'Enter postgres password: ' password
-PGPASSWORD=${password} psql -h localhost -p 5430 -d postgres -U postgres -a -q -f ./sql/data.sql
+PGPASSWORD=${password} psql -h localhost -p 5432 -d postgres -U postgres -a -q -f ./sql/data.sql
 
 ./gradlew bootRun & ./gradlew bank:bootRun && fg
 
