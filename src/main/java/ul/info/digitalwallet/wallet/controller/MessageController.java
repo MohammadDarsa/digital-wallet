088a9bd9a6cd4b08f89fe3e0aa34ad82b61ad516
darsa <darsafxz@gmail.com>
2023-02-28 22:38:35 +0200

    added cors to MessageController

--- src/main/java/ul/info/digitalwallet/wallet/controller/MessageController.java
+++ src/main/java/ul/info/digitalwallet/wallet/controller/MessageController.java
@@ -1,16 +1,14 @@
 package ul.info.digitalwallet.wallet.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ul.info.digitalwallet.common.models.Message;
 import ul.info.digitalwallet.common.service.MessageService;
 import ul.info.digitalwallet.common.service.UserService;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequiredArgsConstructor
 public class MessageController {
