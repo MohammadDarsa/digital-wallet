a3fff7ae9fa6b02375c65016e5f95bc795373f8c
FatimaBaher <112431464+FatimaBaher@users.noreply.github.com>
2023-02-28 23:04:39 +0200

    profile

--- litPay/assets/js/accuont.js
+++ litPay/assets/js/accuont.js
@@ -1,10 +1,25 @@
-const username = document.querySelector("#username");
-const email = document.querySelector("#email");
-const lname = document.querySelector("#lname");
-const fname = document.querySelector("#fname");
-const password = document.querySelector("#password");
-
-const getProfileInfo = () => {
-    // implement profile first...
-    // const res = sendPostRequest()
+let data;
+let usernameInp = document.querySelector("#username");
+let fnameInp = document.querySelector("#fname");
+let lnameInp = document.querySelector("#lname");
+let mobileInp = document.querySelector("#phone");
+let emailInp = document.querySelector("#email");
+async function getData(){
+    let result = await sendRequestGet("/wallet/get-wallet-details");
+    data = result.response;
 }
+
+function setData(){
+    //
+}
+const getProfileInfo = async () => {
+    await getData();
+    usernameInp.value = data.wallet.user.username;
+    fnameInp.value = data.profile.firstName;
+    lnameInp.value = data.profile.lastName;
+    mobileInp.value = data.profile.mobileNumber;
+    emailInp.value = data.wallet.user.email;
+
+}
+
+getProfileInfo();
